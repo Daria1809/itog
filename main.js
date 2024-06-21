167e7143ced9572696aba430601e7014cf4ae7c3
Ivanova Daria <IvanovaDV22@st.ithub.ru>
2024-06-21 14:06:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+for (let li of list.querySelectorAll('li')) {
+    let span = document.createElement('span'); 
+    span.classList.add('show');
+    li.prepend(span); 
+    span.append(span.nextSibling) 
+}
+ list.onclick = function(event) {
+    if (event.target.tagName != 'SPAN') return;
+
+    let childrenList = event.target.parentNode.querySelector('ul');
+    if(!childrenList){
+        alert("Подробнее можете прочитать на этом сайте:");
+        return;
+    }
+    childrenList.hidden = !childrenList.hidden;
+
+    if (childrenList.hidden) {
+        event.target.classList.add('hide');
+        event.target.classList.remove('show');
+    }
+
+    else {
+        event.target.classList.add('show');
+        event.target.classList.remove('hide');
+    }
+}  
+
